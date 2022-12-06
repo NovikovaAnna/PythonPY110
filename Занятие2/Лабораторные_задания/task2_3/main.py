64f0bcab6b7715d8b1318c7a23e6db1fb1140b24
Novikova.Anna <nna.novikova19912020@gmail.com>
2022-12-06 21:00:41 +0300

    lab2_done

---
+++ Занятие2/Лабораторные_задания/task2_3/main.py
@@ -0,0 +1,12 @@
+def pow_gen(base: int):
+    a = 0
+    while True:
+        yield base ** a
+        a += 1
+
+
+if __name__ == "__main__":
+    pow_numbers = pow_gen(10)
+
+    for _ in range(5):
+        print(next(pow_numbers))
